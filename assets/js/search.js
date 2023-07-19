c4b46040a6e1cb24856500b1ad84b61ea659b16a
YOKE <mfuu1998@gmail.com>
2023-07-19 16:20:47 +0800

    `bing` support

--- assets/js/search.js
+++ assets/js/search.js
@@ -18,21 +18,40 @@ window.google = {
     },
   },
 };
+window.bing = {
+  sug(data) {
+    const list = data.AS.Results.reduce((res, item) => {
+      res.push(...item.Suggests.map(el => el.Txt));
+      return res;
+    }, [])
+    const suggests = list.map((item) => {
+      return `<li><i class="fa-solid fa-magnifying-glass suggest-icon"></i><div id="text-container">${item}</div></li>`;
+    });
+    handleSuggestWords(suggests);
+  }
+}
 
 function getSuggestDom() {
   return $("#search-tabContent").children(".active").find("#search-suggest");
 }
 
 function getInputDom() {
-  return $("#search-tabContent").children(".active").find("input").eq(0);
+  return $("#search-tabContent").children(".active").find("#searchInput");
+}
+
+function togglePaneClass(bool) {
+  let activePane = $("#search-tabContent").children(".active");
+  activePane[bool ? 'addClass' : 'removeClass']('sug-show');
 }
 
 function handleSuggestWords(suggests = []) {
   let suggest = getSuggestDom();
   suggest.empty().show();
+  togglePaneClass(true);
   if (!suggests.length) {
     suggest.empty();
     suggest.hide();
+    togglePaneClass(false);
   }
   suggests.forEach((item) => suggest.append(item));
 }
@@ -45,7 +64,7 @@ function onInputChange() {
   }
   let suggest = $(this).attr("data-suggest");
   let jsonp = $(this).attr("data-jsonp");
-  let callback = suggest.match(eval(`/${jsonp}=(.*)/`))[1];
+  let callback = $(this).attr("data-callback");
   $.ajax({
     url: suggest.replace("#content#", keywords),
     dataType: "jsonp",
@@ -59,36 +78,48 @@ function onInputChange() {
   });
 }
 
+function onSearch() {
+  let input = getInputDom();
+  if (!input) {
+    console.error('The Input box with id `searchInput` is not found.')
+    return;
+  }
+  let word = input.val();
+  if (!word.trim()) {
+    return;
+  }
+  let search = input.attr("data-search") + word;
+  location.href = search;
+}
+
 $(function () {
   $(document).keyup(function (event) {
     if (event.keyCode == 13) {
-      let input = getInputDom();
-      let word = input.val();
-      if (!word.trim()) {
-        return;
-      }
-      let search = input.attr("data-search") + word;
-      location.href = search;
+      onSearch();
     }
   });
   $(document).on("click", "#search-suggest li", function () {
     let word = $(this).find("#text-container").text();
     let input = getInputDom();
-    let search = input.attr("data-search") + word;
     input.val(word);
-    location.href = search;
+    onSearch();
   });
-  $(document).on("click", "input", function (e) {
+  $(document).on("click", "#searchInput", function (e) {
     e.stopPropagation();
     let suggest = getSuggestDom();
     if (suggest.children().length) {
       suggest.show();
+      togglePaneClass(true);
     }
+  });
+  $(document).on("click", "#submitInput", function (e) {
+    e.stopPropagation();
+    onSearch();
   });
   $(document).on("click", function (e) {
     e.stopPropagation();
     let suggest = getSuggestDom();
     suggest.hide();
+    togglePaneClass(false);
   });
 });
-  
